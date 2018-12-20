8eaa7e4e8c7a29c9a84f559978c9a46ca2145c13
RyanAchten <ryanachten@gmail.com>
2018-12-21 09:38:25 +1300

    Add json server and test db data

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,14 @@
-const graphql = require('graphql');
-const _ = require('lodash');
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema,
-} = graphql;
+const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
+  { id: "23", firstName: "Bill", age: 20 },
+  { id: "47", firstName: "Samantha", age: 21 }
 ];
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
@@ -22,19 +17,19 @@ const UserType = new GraphQLObjectType({
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     user: {
       type: UserType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
         return _.find(users, { id: args.id });
       }
     }
   }
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery
